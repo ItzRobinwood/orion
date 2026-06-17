b2fecacafff64ffa332c42c81015eedc7a265fc3
CaioNasc18 <caio.nascimento.1702@gmail.com>
2026-06-17 15:39:01 +0100

    Update managerDashboard.jsx

--- Project/frontend/src/pages/managerDashboard.jsx
+++ Project/frontend/src/pages/managerDashboard.jsx
@@ -1,1 +1,10 @@
+function ManagerDashboard() {
+    return (
+        <div>
+            <h1>Manager Dashboard</h1>
+            <p>Em construção...</p>
+        </div>
+    );
+}
+
 export default ManagerDashboard;
