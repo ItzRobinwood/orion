1c5b2f5646baf71e7b7dc28a8db65f094b123da3
CaioNasc18 <caio.nascimento.1702@gmail.com>
2026-07-01 23:50:49 +0100

    Update companiesRoutes.js

--- Project/backend/src/routes/companiesRoutes.js
+++ Project/backend/src/routes/companiesRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require('../controllers/companiesController');
 const { verifyToken, verifyAdmin } = require('../middleware/auth');
 
-router.get('/companies', verifyToken, verifyAdmin, controller.getCompanies);
+router.get('/companies', verifyToken, controller.getCompanies);
 router.post('/companies', verifyToken, verifyAdmin, controller.createCompany);
 router.put('/companies/:id', verifyToken, verifyAdmin, controller.updateCompany);
 router.delete('/companies/:id', verifyToken, verifyAdmin, controller.deleteCompany);
